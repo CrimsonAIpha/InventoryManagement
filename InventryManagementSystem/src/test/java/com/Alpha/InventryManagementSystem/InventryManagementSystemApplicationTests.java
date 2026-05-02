1ddd461d2e45f55fedbf8ed91eba95d158779523
yashc <yashc@192.168.1.38>
2026-05-02 22:53:31 +0530

    SpringBoot Architercture  and all dependencies added

---
+++ InventryManagementSystem/src/test/java/com/Alpha/InventryManagementSystem/InventryManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Alpha.InventryManagementSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventryManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
